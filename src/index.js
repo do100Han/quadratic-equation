395ab331ce84b026ab96d0e10c6f1b6b537bec09
Rutkevich Sasha <rutkevich.sawa@gmail.com>
2018-09-17 23:35:58 +0300

    work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var new_a = equation.replace(/\s/g, ''),
+    arr = new_a.match(/(\-|\+)?[^\^](\d+)/g),
+    a = parseInt(arr[0]),
+    b = parseInt(arr[1]),
+    c = parseInt(arr[2]),
+    D = Math.pow(b, 2) - 4 * a * c,
+    mas = [],
+    x1 = Math.round((-b - Math.sqrt(D)) / (2 * a)),
+    x2 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+    if (x1 < x2) {
+      mas[0] = x1;
+      mas[1] = x2;
+    } else {
+      mas[0] = x2;
+      mas[1] = x1;
+    }
+  return mas;
 }
